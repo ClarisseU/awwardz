3c48a2194b64b05df49c83c1faf406c24874d42f
ClarisseU <clarisseum@gmail.com>
2019-10-25 21:15:09 +0200

    add merch url

--- ardz/urls.py
+++ ardz/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+    url(r'^new/post$',views.new_post, name ='new-post'),
+    url(r'^search',views.search, name='search'),
+    url(r'^new/newprofile$',views.new_profile, name ='new-profile'),
+    url(r'^new/profile$',views.profile, name ='profile'),
+    url(r'^api/merch/$', views.ProfileList.as_view(), name='profile_api'),
+    url(r'^api/merch1/$', views.ProjectList.as_view(), name='project_api'),
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
