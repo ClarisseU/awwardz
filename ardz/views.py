04c4c865dd671e01ab617170fb7f9e547c43bb3d
ClarisseU <clarisseum@gmail.com>
2019-10-25 13:36:33 +0200

    add welcome method

--- ardz/views.py
+++ ardz/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import Profile, Projects
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def welcome(request):
+    project = Projects.objects.all()
+    return render(request, 'index.html')
+    
